7d0656e4ae589fe7e6508bd15110ccef212b5f53
sudipX <sudippoudel@sd.taylors.edu.my>
2023-03-30 00:17:27 +0545

    needs more optimization

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
+import os.path
 from pytube import YouTube
 import pyperclip
+
 print("Go & Copy Your Link First !!!")
-link = pyperclip.waitForNewPaste()
+link = pyperclip.waitFornPaste()
 print("Your Song Title:")
-print("_"*40)
+print("_"*100)
 video = YouTube(link)
 print(video.title)
+print("_"*100)
 correct = input("Proceed ? (y / n):\n>> ").lower()
 if correct == "y":
+    dir = input("Choose Your Directory To Save Video:\n>> ")
     stream = video.streams.get_highest_resolution()
-    stream.download()
+    output = stream.download()
+    video_path = os.path(dir, output)
+    final_video = open(video_path, "w")
