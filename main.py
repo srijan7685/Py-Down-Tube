60ed7bf98397b89cc136b341e3e7d88e7fd6e58d
sudipX <sudippoudel@sd.taylors.edu.my>
2023-03-29 23:58:20 +0545

    prototype complete

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from pytube import YouTube
 
-link = input("Enter the link:\n>> ")
+link = "https://youtu.be/b5WdL51te0A"
 my_video = YouTube(link)
 
 # Getting video title
 print(my_video.title)
+
+# Getting url thumbnail
+#print(my_video.thumbnail_url)
+
+# getting streams
+print(my_video.streams.filter(progressive=True))
+
